f35911b18ab011c18b5367f004880986c59082e7
Alisson Bruno <alissonbruno.sa@gmail.com>
2017-04-02 22:04:56 -0300

    Add appender

---
+++ appender.go
@@ -0,0 +1,42 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"runtime"
+)
+
+const (
+	Cyan   = 36
+	Yellow = 33
+	Red    = 31
+	Reset  = 0
+)
+
+const (
+	DEBUG = "DEBUG"
+	INFO  = "INFO"
+	WARN  = "WARN"
+	ERROR = "ERROR"
+)
+
+type Appender struct {
+	Output io.Writer
+}
+
+func (a *Appender) log(logType string, color int, mensage interface{}) {
+	fmt.Fprintf(a.Output, "%s[%s]\t- [%s] %v %s \n", format(color), logType, file(), mensage, reset())
+}
+
+func file() string {
+	_, file, _, _ := runtime.Caller(3)
+	return file
+}
+
+func format(sequence int) string {
+	return fmt.Sprintf("\x1b[%d;%dm", 0, sequence)
+}
+
+func reset() string {
+	return format(Reset)
+}
